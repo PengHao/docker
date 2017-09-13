3f64d82734c662e106ee1e10a117bcde92689198
wolfpeng <penghao345@gmail.com>
2017-09-14 00:59:40 +0800

    fix build project error

--- basedocker/makeproject.sh
+++ basedocker/makeproject.sh
@@ -1,4 +1,8 @@
+echo $1
+timestamp=`date +%s`
+echo $timestamp
+#docker rmi -f wolfpeng/project:1.0.0
 mv DockerfileProject Dockerfile
-docker build -t wolfpeng/project:1.0.0 .
+docker build -t wolfpeng/$1:${timestamp} .
 mv Dockerfile DockerfileProject
-docker run -i -t -p80:80 wolfpeng/project:1.0.0
+docker run -i -t -p80:80 wolfpeng/$1:${timestamp}
